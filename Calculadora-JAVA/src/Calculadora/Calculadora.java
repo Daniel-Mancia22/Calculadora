7d54a7aac3b6a129084a0ea61a02fabf8b714b30
Daniel-Mancia22 <danielmancia368@gmail.com>
2025-06-21 12:57:16 -0600

    Calculadora Java - Terminada

---
+++ Calculadora-JAVA/src/Calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package Calculadora;
+
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+      VistaCalculadora v = new VistaCalculadora();
+      v.setVisible(true);
+    } 
+}
